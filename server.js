fec615e8bec098d9191013c55ec94285fcef4e43
roy_benjamin <benji01560@gmail.com>
2016-08-25 04:48:42 +0000

    Project done

--- server.js
+++ server.js
@@ -1,48 +1,14 @@
 var express = require('express');
+var moment = require('moment');
 var app = express();
+var path    = require("path");
 var port = 8080;
-//var m_naturalDate = "";
-//var m_unixDate = "";
+var m_Date = null;
 
-function dateIsValid(date){
-    var months = ['January','February','Mars','April','May','June','July','August','September','October','November','December'];
-    var dateArray = date.split(" ");
-    
-    var monthNum = months.indexOf(dateArray[0]) + 1;
-    if(monthNum < 1){
-        return -1;
-    }
-    
-    console.log('Month: ' + monthNum);
-    console.log('Day: '+ dateArray[1]);
-    console.log('Year: '+ dateArray[2]);
-    
-    var naturalDate = dateArray[2] + '.' + monthNum + '.' + dateArray[1];
-    return naturalDate;
-}
+app.use(express.static(__dirname));
 
-function convertUnixToNaturalDate(unixDate){
-    // Thanks to http://stackoverflow.com/questions/847185/convert-a-unix-timestamp-to-time-in-javascript for the solution:
-    var date = new Date(unixDate * 1000);
-    var months = ['January','February','Mars','April','May','June','July','August','September','October','November','December'];
-    
-    var year = date.getFullYear();
-    var monthStr = months[date.getMonth()];
-    var monthNum = date.getMonth();
-    var day = date.getDate();
- 
-    var naturalDate = year + '.' + monthNum + '.' + day;
-    return naturalDate;
-}
-
-function convertNaturalDateToUnix(naturalDate){
-    var unixDate = new Date(naturalDate).getTime() / 1000; // '2012.08.10'
-    return unixDate.toString();
-}
-
-app.all("*", function(req, res, next) {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  next();
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html')); // Render HTML File
 });
 
 app.get("/:datestring", function(req, res) {
@@ -52,25 +18,28 @@ app.get("/:datestring", function(req, res) {
     if(isNaN(date)){
         
         console.log('The string contains a natural language date');
-        var naturalDate = dateIsValid(date);
-        var unixDate = convertNaturalDateToUnix(naturalDate);
-        res.end(unixDate);
+        m_Date = moment(date, 'MMMM D, YYYY');
     }
     
     // The string contains a unix timestamp date
     else{  
         
         console.log('The string contains a unix timestamp date');
-        var naturalDate = convertUnixToNaturalDate(date);
-        var unixDate = date;
-        res.end(naturalDate);
+        m_Date = moment(date, 'X');
     }
     
-});
-
-app.get("/", function(req, res) {
-    console.log('ERROR 404!');
-    res.end("ERROR 404!");
+    if(m_Date.isValid()){
+        res.json({
+            unix: m_Date.format('X'),
+            natural: m_Date.format('MMMM D, YYYY')
+        }); 
+    }
+    else{
+        res.json({
+            unix: null,
+            natural: null
+        });   
+    }
 });
 
 app.listen(port, function(){
